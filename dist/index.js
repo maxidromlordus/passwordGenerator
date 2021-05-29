febd661430499c5d0d0ca8d2f212c3cfc5288b88
Maxim Samohvalov <maxidromlordus@gmail.com>
2021-05-29 12:36:09 +0300

    html and css template

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("init"); //
+
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var upCase = lowCase.toLocaleUpperCase();
+var numbers = "1234567890";
+console.log(upCase);
+var simb = "~`!@#$%^&*()_-+={[}]|:;<,>.?/";
---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log("init"); //
+
+let lowCase = "abcdefghijklmnopqrstuvwxyz";
+let upCase = lowCase.toLocaleUpperCase();
+let numbers = "1234567890";
+console.log(upCase);
+let simb = "~`!@#$%^&*()_-+={[}]|:;<,>.?/";
