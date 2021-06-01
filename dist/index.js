59d7e91fe52cce394269b50faffeeab6d30f0fc2
Maxim Samohvalov <maxidromlordus@gmail.com>
2021-06-01 22:25:46 +0300

    finished

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,86 @@ var lowCase = "abcdefghijklmnopqrstuvwxyz";
 var upCase = lowCase.toLocaleUpperCase();
 var numbers = "1234567890";
 console.log(upCase);
-var simb = "~`!@#$%^&*()_-+={[}]|:;<,>.?/";
+var simb = "~`!@#$%^&*()_-+={[}]|:;<,>.?/"; // basic function to generate random numbers
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+} // Function to replace alphabetic chars with special chars
+
+
+var replaceSpecialChar = function replaceSpecialChar(pass) {
+  var passLenght = pass.length;
+  var passPosition = getRandomInt(0, passLenght - 1);
+  var specialCharPossition = getRandomInt(0, simb.length - 1);
+  var selectedChar = simb[specialCharPossition];
+  pass[passPosition] = selectedChar;
+  console.log("replaced ".concat(passPosition, " char"));
+}; // main function to generate password
+
+
+var generatePass = function generatePass() {
+  var genPass = "";
+  var charsLenght = document.getElementsByName("lenght");
+  var checkedValue = undefined;
+  var valuePromise = new Promise(function (resolve, reject) {
+    for (var i = 0; i < charsLenght.length; i++) {
+      if (charsLenght[i].checked) {
+        checkedValue = charsLenght[i].value;
+      }
+    }
+
+    resolve(checkedValue);
+  });
+  valuePromise.then(function (result) {
+    for (var i = 0; i < +result; i++) {
+      var ch = getRandomInt(0, lowCase.length - 3);
+      genPass += lowCase[ch];
+    }
+
+    return new Promise(function (resolve) {
+      var arrayPass = genPass.split("");
+      resolve(arrayPass);
+    });
+  }).then(function (result) {
+    var withSimbols = document.querySelector(".simbols");
+
+    if (withSimbols.checked) {
+      for (var i = 0; i < 3; i++) {
+        replaceSpecialChar(result);
+      }
+
+      var finalResult = result.join("");
+      var displayResult = document.querySelector(".generated-text");
+      displayResult.value = finalResult;
+      var text = displayResult.innerText;
+      displayResult.focus();
+      displayResult.select();
+      displayResult.blur();
+      document.execCommand("copy");
+    } else {
+      var _finalResult = result.join("");
+
+      var _displayResult = document.querySelector(".generated-text");
+
+      _displayResult.value = _finalResult;
+      var _text = _displayResult.innerText;
+
+      _displayResult.focus();
+
+      _displayResult.select();
+
+      _displayResult.blur();
+
+      document.execCommand("copy");
+    }
+  }); //   if (checkedValue) {
+  //
+  // }
+};
+
+var generateButton = document.querySelector(".button");
+generateButton.addEventListener("click", function () {
+  generatePass();
+});
--- src/index.js
+++ src/index.js
@@ -5,3 +5,80 @@ let upCase = lowCase.toLocaleUpperCase();
 let numbers = "1234567890";
 console.log(upCase);
 let simb = "~`!@#$%^&*()_-+={[}]|:;<,>.?/";
+
+// basic function to generate random numbers
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+// Function to replace alphabetic chars with special chars
+const replaceSpecialChar = function (pass) {
+  const passLenght = pass.length;
+  const passPosition = getRandomInt(0, passLenght - 1);
+  const specialCharPossition = getRandomInt(0, simb.length - 1);
+  const selectedChar = simb[specialCharPossition];
+
+  pass[passPosition] = selectedChar;
+  console.log(`replaced ${passPosition} char`);
+};
+
+// main function to generate password
+const generatePass = function () {
+  let genPass = "";
+  const charsLenght = document.getElementsByName("lenght");
+  let checkedValue = undefined;
+  const valuePromise = new Promise((resolve, reject) => {
+    for (let i = 0; i < charsLenght.length; i++) {
+      if (charsLenght[i].checked) {
+        checkedValue = charsLenght[i].value;
+      }
+    }
+    resolve(checkedValue);
+  });
+
+  valuePromise
+    .then((result) => {
+      for (let i = 0; i < +result; i++) {
+        const ch = getRandomInt(0, lowCase.length - 3);
+        genPass += lowCase[ch];
+      }
+      return new Promise((resolve) => {
+        let arrayPass = genPass.split("");
+        resolve(arrayPass);
+      });
+    })
+    .then((result) => {
+      const withSimbols = document.querySelector(".simbols");
+      if (withSimbols.checked) {
+        for (let i = 0; i < 3; i++) {
+          replaceSpecialChar(result);
+        }
+        let finalResult = result.join("");
+        const displayResult = document.querySelector(".generated-text");
+        displayResult.value = finalResult;
+        const text = displayResult.innerText;
+        displayResult.focus();
+        displayResult.select();
+        displayResult.blur();
+        document.execCommand("copy");
+      } else {
+        let finalResult = result.join("");
+        const displayResult = document.querySelector(".generated-text");
+        displayResult.value = finalResult;
+        const text = displayResult.innerText;
+        displayResult.focus();
+        displayResult.select();
+        displayResult.blur();
+        document.execCommand("copy");
+      }
+    });
+  //   if (checkedValue) {
+  //
+  // }
+};
+
+const generateButton = document.querySelector(".button");
+generateButton.addEventListener("click", () => {
+  generatePass();
+});
